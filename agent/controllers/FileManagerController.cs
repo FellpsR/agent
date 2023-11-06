ae671988200df25b7cc0aced856054df0a4c60a3
Felps <feliperodriguesrdo@gmail.com>
2023-11-06 17:59:19 -0300

    Tentativa de validação parcial

--- agent/controllers/FileManagerController.cs
+++ agent/controllers/FileManagerController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.Runtime.InteropServices;
+using System.Security.AccessControl;
 using FileManager.Model;
 
 [Route("link")]
@@ -25,6 +26,27 @@ public class FileManagerController : ControllerBase
             {
                 try
                 {
+                    // Verifica as permissões do diretório ou arquivo.
+                    var accessControl = System.IO.File.Exists(path) ? (FileSystemSecurity)new FileSecurity(path, AccessControlSections.Access) : new DirectorySecurity(path, AccessControlSections.Access);
+                    var rules = accessControl.GetAccessRules(true, true, typeof(System.Security.Principal.NTAccount));
+
+                    // Verifica se o usuário atual tem permissão para acessar o diretório ou arquivo.
+                    var currentUser = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+                    var hasPermission = false;
+                    foreach (FileSystemAccessRule rule in rules)
+                    {
+                        if (rule.IdentityReference.Value.Equals(currentUser, StringComparison.CurrentCultureIgnoreCase) && rule.AccessControlType == AccessControlType.Allow)
+                        {
+                            hasPermission = true;
+                            break;
+                        }
+                    }
+
+                    if (!hasPermission)
+                    {
+                        return Forbid();
+                    }
+
                     // Se o caminho é um arquivo, ele é aberto aqui.
                     Process.Start(new ProcessStartInfo
                     {
@@ -35,9 +57,9 @@ public class FileManagerController : ControllerBase
                 }
                 catch (SystemException ex)
                 {
-                    var msg = "Erro ao abrir arquivo. "+ex.Message;
-                    System.Console.WriteLine(msg);
-                    return BadRequest(new {message = msg});
+                    var msg = "Erro ao abrir arquivo. " + ex.Message;
+                    Console.WriteLine(msg);
+                    return BadRequest(new { message = msg });
                 }
             }
             else
@@ -49,6 +71,7 @@ public class FileManagerController : ControllerBase
         catch (Exception ex)
         {
             // Lidar com exceções, se necessário.
-            return StatusCode(500, "Teste de msg de retorno! "+ex.Message);
+            return StatusCode(500, "Teste de msg de retorno! " + ex.Message);
         }
-    }}
+    }
+}
